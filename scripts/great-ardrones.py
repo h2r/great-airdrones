149f4760ad97096a396f44bd91c0a3548ac92179
Josh Roy <joshnroy@gmail.com>
2016-03-19 22:20:17 -0400

    Fix some style errors

--- scripts/great-ardrones.py
+++ scripts/great-ardrones.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('great-ardrones')
+
+import sys
+import select
+import termios
+import tty
+import time
+
+import roslib
+roslib.load_manifest('great-ardrones')
 import rospy
 
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Empty
-from std_msgs.msg import String
-
-import sys, select, termios, tty, time, numpy as np
+from geometry_msgs.msg import PoseStamped
 
 
 def getKey():
@@ -17,33 +23,21 @@ def getKey():
     return key
 
 
-global velocityTStamp
 velocityTStamp = ()
-goal = np.array([0, 0, 0])
-global oldLoc
-oldLoc = np.array([1, 1, 1])
+goal = None
+
+gamma = 0.01
+bound = 0.001
 
 def callback(data):
-    print "received", data
-    rospy.loginfo("Current Location %s",data.data)
-    cur = np.array(data.data)
-    if np.sum(np.abs(cur-oldLoc))==0:
-    	land_pub.publish(Empty())
-    	print "out of motion capture rig"
-    else:
-	   global oldLoc
-	   oldLoc = cur
-	   gamma = 0.1
-	   velocities = (goal-cur)*gamma
-	   bound = 0.5
-	   velocities[velocities>bound] = bound
-	   velocities[velocities<-bound] = -bound
-	   global velocityTStamp
-	   velocityTStamp = (velocities, time.clock())
+    cur = [data.pose.position.x, data.pose.position.y, data.pose.position.z]
+    if goal is not None:
+	velocities = [gamma*(i - j) for i,j in zip(goal, cur)]
+	velocities] = [min(val, bound)  for val in velocities]
+	velocities] = [max(val, -bound) for val in velocities]
+	velocityTStamp = (velocities + time.clock())
 
 
-
-print "1"
 settings = termios.tcgetattr(sys.stdin)
 
 pub = rospy.Publisher('cmd_vel', Twist)
@@ -52,48 +46,52 @@ reset_pub = rospy.Publisher('/ardrone/reset', Empty)
 takeoff_pub = rospy.Publisher('/ardrone/takeoff', Empty)
 
 rospy.init_node('great-ardrones')
-#<<<<<<< HEAD
-rospy.Subscriber("vrpn_client_node/Drone/pose", String, callback)
-#=======
-#rospy.Subscriber("vrpn_ros_node/Drone/pose", String, callback)
-#>>>>>>> 6e04459de384aaba61d3c781e97f6a8752e5b2b9
-print "2"
+print rospy.Subscriber("/vrpn_client_node/Drone/pose", PoseStamped, callback)
 
 try:
     while(True):
+        print velocityTStamp
         key = getKey()
+
         # takeoff and landing
         print key
-        if key=="q":
+        if key == "q":
             quit()
-        if key == 'l':
+        elif key == 'l':
             land_pub.publish(Empty())
         elif key == 't':
             takeoff_pub.publish(Empty())
 
         twist = Twist()
-        if key=="g":
+        if key == "g":
             key = getKey()
             cur = ""
             temp = []
-            while key!="e" and len(temp)<3:
-                if key=="-" or key.isdigit():
+
+            while key != "e" and len(temp) < 3:
+                if key == "-" or key.isdigit() or key == ".":
                     cur += key
-                elif key==" ":
+                elif key == " ":
                     try:
-                        temp.append(int(cur))
+                        temp.append(float(cur))
                     except Exception as e:
                         print e
+
                     cur = ""
                 key = getKey()
-            if len(temp)==3:
-                goal = np.array(temp)
+
+            if len(temp) == 3:
+                goal = temp
             else:
-                print "new goal incorrectly formatted", temp
-            print goal
-        elif len(velocityTStamp)==2 and time.clock()-velocityTStamp[1]<1:
-        	LINEAR = twist.LINEAR
-                LINEAR.x, LINEAR.y, LINEAR.z = velocityTStamp[0]
+                print("New goal incorrectly formatted %.6f" % temp)
+
+            print("Goal is %.6f" % goal)
+
+        elif len(velocityTStamp) == 2 and time.clock() - velocityTStamp[3] < 1:
+            twist.linear.x = velocityTStamp[0]
+            twist.linear.y = velocityTStamp[1]
+            twist.linear.z = velocityTStamp[2]
+
         print twist
         pub.publish(twist)
 
