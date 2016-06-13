fb6273ea2803e660b13d17b8d7b37436a0f725bb
Josh Roy <joshnroy@gmail.com>
2016-06-13 15:44:11 -0400

    Removed wifi need from lsdslamcore

--- scripts/lsdslamcore.sh
+++ scripts/lsdslamcore.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ `iwgetid -r` == 'arachna' ]]; then
     if [[ `rostopic list | grep /ardrone/front/image_rect` == "" ]]; then
         echo "No image stream available;("
         exit 1;
@@ -8,8 +7,5 @@ if [[ `iwgetid -r` == 'arachna' ]]; then
 
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     rosrun lsd_slam_core live_slam /image:=/ardrone/front/image_rect _calib:=$DIR/calibration.cfg &>/dev/null &
-else
-    echo "You aren't connected to arachna network"
-fi
 
 exit 0;
